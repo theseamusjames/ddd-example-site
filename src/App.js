ebf7497c6e923f71ac1a940b3b7f3129115a9b02
Seamus <seamus@encdr.com>
2022-01-18 22:46:06 -0800

    React router added and header created.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <ul>
+          <li>
+            <h1>Bridge & Glass</h1>
+          </li>
+          <li>
+            <Link to="/category/mens">Mens</Link>
+          </li>
+          <li>
+            <Link to="/category/womens">Womens</Link>
+          </li>
+        </ul>
       </header>
     </div>
   );
