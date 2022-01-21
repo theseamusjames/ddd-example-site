bc5d31afd9deabbec33024bb6efcea0a2c23310a
Seamus <seamus@encdr.com>
2022-01-20 23:21:15 -0800

    Created the product page and gallery components. Moved product loading to app.js

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,16 @@
 import "./ProductCard.css";
+import {Link} from 'react-router-dom';
 
 export default function ProductCard({product}) {
     return (
         <div className="productCard">
-            <img src={`/assets/${product.images[0]}`} alt={product.name}/>
-            <div className="productInfo">
-                <h3>{product.name}</h3>
-                <p>${product.price}</p>
-            </div>
+            <Link to={`/product/${product.id}`}>
+                <img src={`/assets/${product.images[0]}`} alt={product.name}/>
+                <div className="productInfo">
+                    <h3>{product.name}</h3>
+                    <p>${product.price}</p>
+                </div>
+            </Link>
         </div>
     );
 }
