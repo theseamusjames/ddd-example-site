51f0b53a054eaaf93431e38c5de54f08e56fd79d
Seamus <seamus@encdr.com>
2022-02-22 23:30:33 -0800

    onChange not onClick for radio buttons on shipping method panel

--- src/components/ShippingMethodPanel.jsx
+++ src/components/ShippingMethodPanel.jsx
@@ -13,10 +13,10 @@ export default function ShippingMethodPanel() {
         <div className='shippingMethodPanel'>
             <h3>Select Shipping Method</h3>
             <div>
-                <input id='standard' data-testid='shippingMethodRadio' type='radio' value='standard' onClick={_setShippingMethod} checked={shippingMethod === 'standard'}/> <label for='standard'>Standard 5-7 Business Days</label>
+                <input id='standard' data-testid='shippingMethodRadio' type='radio' value='standard' onChange={_setShippingMethod} checked={shippingMethod === 'standard'}/> <label htmlFor='standard'>Standard 5-7 Business Days</label>
             </div>
             <div>
-                <input id='express' data-testid='shippingMethodRadio' type='radio' value='express' onClick={_setShippingMethod} checked={shippingMethod === 'express'}/> <label for='express'>Express 1-2 Business Days</label>
+                <input id='express' data-testid='shippingMethodRadio' type='radio' value='express' onChange={_setShippingMethod} checked={shippingMethod === 'express'}/> <label htmlFor='express'>Express 1-2 Business Days</label>
             </div>
         </div>
     );
